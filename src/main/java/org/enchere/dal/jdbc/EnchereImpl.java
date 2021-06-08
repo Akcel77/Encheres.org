be4a00222bbaae5b380b7773fa548c31b3c7560a
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-06-08 21:12:04 +0200

    Enchere BO / Impl / DAO / Manager Floriane

--- src/main/java/org/enchere/dal/jdbc/EnchereImpl.java
+++ src/main/java/org/enchere/dal/jdbc/EnchereImpl.java
@@ -6,14 +6,17 @@ import org.enchere.dal.EnchereDAO;
 import org.enchere.outils.BusinessException;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnchereImpl implements EnchereDAO {
         private static final String INSERT = "INSERT INTO ENCHERES(date_enchere, montant_enchere, " +
                 "no_article, no_utilisateur) VALUES(?,?,?,?);";
-        private static final String UPDATE = "UPDATE ENCHERES SET date_enchere=?, montant_enchere=?, \" +\n" +
-                "                \"no_article=?, no_utilisateur=? WHERE no_enchere=?";
-
-        private static final String DELETE = "SELECT * FROM enchere WHERE no_enchere=?";
+        private static final String UPDATE = "UPDATE ENCHERES SET date_enchere=?, montant_enchere=?, no_article=?, " +
+                "no_utilisateur=? WHERE no_enchere=?;";
+        private static final String DELETE = "SELECT * FROM enchere WHERE no_enchere=?;";
+        private static final String FIND_ALL= "SELECT * FROM enchere;";
+        private static final String FIND_BY_ID = "SELECT * FROM enchere WHERE no_enchere=?;";
 
         /**
          * Insérer une nouvelle enchère
@@ -71,6 +74,51 @@ public class EnchereImpl implements EnchereDAO {
                 }
         }
 
+        @Override
+        public List<Enchere> findAll() throws SQLException{
+                List<Enchere> listEnchere = new ArrayList<>();
+                try {
+                        Connection cnx = ConectionProvider.getConnection();
+                        PreparedStatement stmt = cnx.prepareStatement(FIND_ALL);
+
+                        ResultSet rs = stmt.executeQuery();
+                        while (rs.next()){
+                                listEnchere.add(new Enchere(
+                                        rs.getInt("no_enchere"),
+                                        rs.getString("date_enchere"),
+                                        rs.getInt("montant_enchere"),
+                                        rs.getInt("no_article"),
+                                        rs.getInt("no_utilisateur")
+
+                                ));
+                        }
+
+
+                } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                }
+
+                return listEnchere;
+        }
+
+        public Enchere find(int no_enchere) throws SQLException{
+                Enchere enchere = null;
+                try {
+                        Connection cnx = ConectionProvider.getConnection();
+                        PreparedStatement stmt = cnx.prepareStatement(FIND_BY_ID);
+                        stmt.setInt(1,no_enchere);
+                        ResultSet rs = stmt.executeQuery();
+                        while (rs.next()){
+                                enchere.setNo_enchere(rs.getInt("no_enchere"));
+                                enchere.setDate_enchere(rs.getString("date_enchere"));
+                                enchere.setMontant_enchere(rs.getInt("montant_enchere"));
+                                enchere.setNo_article(rs.getInt("no_article"));
+                                enchere.setNo_utilisateur(rs.getInt("no_utilisateur"));
+                        }
+                } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                }
+                return enchere;
+        }
 
 }
-
