02e11e8c544d58990f4548c6cc9404226c990bdd
Floriane <floriane.debernardi2021@campus-eni.fr>
2021-06-08 19:04:50 +0200

    encheres(impl, dao, bo, manager)

--- src/main/java/org/enchere/dal/EnchereDAO.java
+++ src/main/java/org/enchere/dal/EnchereDAO.java
@@ -1,11 +1,16 @@
 package org.enchere.dal;
 
+import org.enchere.bo.Articles;
 import org.enchere.bo.Enchere;
 import org.enchere.outils.BusinessException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public interface EnchereDAO {
     public void create(Enchere enchere)throws BusinessException;
     public void update(Enchere enchere)throws BusinessException;
     public void delete (Enchere enchere) throws BusinessException;
-
+    public List<Enchere> findAll() throws SQLException;
+    public Enchere find(int no_enchere) throws SQLException;
 }
