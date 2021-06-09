4dab4db9a0d1bb21700184ce4317f9e2f93cb871
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-06-09 22:31:01 +0200

    Inscription Ok Servlet Operationnel Enregistrement en base de donnee ok

--- src/main/java/org/enchere/servlet/log/ServletInscription.java
+++ src/main/java/org/enchere/servlet/log/ServletInscription.java
@@ -1,9 +1,14 @@
 package org.enchere.servlet.log;
 
+import org.enchere.bll.UtilisateurManager;
+import org.enchere.bo.Utilisateur;
+import org.enchere.outils.BusinessException;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "ServletInscription", value = "/Inscription")
 public class ServletInscription extends HttpServlet {
@@ -17,7 +22,62 @@ public class ServletInscription extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Todo gestion de l'inscription
-        doGet(request,response);
+        //Cree un Utilisateur
+        //Check si tous les champs sont remplis
+        //Si c'est ok cree un Utilisateur
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/inscription.jsp");
+        Utilisateur utilisateur = null;
+
+        try {
+            String pseudo = request.getParameter("pseudo");
+            String nom = request.getParameter("nom");
+            String prenom = request.getParameter("prenom");
+            String email = request.getParameter("email");
+            String password = request.getParameter("psw");
+            String passwordConf = request.getParameter("pswConf");
+            String telephone = request.getParameter("telephone");
+            String rue = request.getParameter("rue");
+            String ville = request.getParameter("ville");
+            String codePostal = request.getParameter("codePostal");
+
+            List<String> listePseudo = UtilisateurManager.AllPseudoList();
+            if (pseudo.length() == 0 || pseudo.isEmpty()){
+                request.setAttribute("erreur", "Veuillez renseigner un pseudo");
+            }else if (listePseudo.contains(pseudo)){
+                request.setAttribute("erreur", "Pseudo deja utilise veuillez renseigner un autre pseudo");
+            }else if (nom.length() == 0 || nom.isEmpty()){
+                request.setAttribute("erreur", "Veuillez renseigner un nom");
+            }else if (prenom.length() == 0 || prenom.isEmpty()){
+                request.setAttribute("erreur", "Veuillez renseigner un prenom");
+            }else if (email.length() == 0 || email.isEmpty()){
+                request.setAttribute("erreur", "Veuillez renseigner un email");
+            }else if (password.length() == 0 || password.isEmpty()) {
+                request.setAttribute("erreur", "Veuillez renseigner un password");
+            }else if (rue.length() == 0 || rue.isEmpty()){
+                request.setAttribute("erreur", "Veuillez renseigner une rue");
+            }else if (ville.length() == 0 || ville.isEmpty()){
+                request.setAttribute("erreur", "Veuillez renseigner une ville");
+            }else if (codePostal.length() == 0 || codePostal.isEmpty()) {
+                request.setAttribute("erreur", "Veuillez renseigner un codePostal");
+            }else if ( passwordConf.equals(password)){
+                utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville,password);
+
+                System.out.println("test Ajout");
+
+                utilisateur = UtilisateurManager.signInUser(utilisateur);
+
+                //Connection de l'utilisateur apres son enregistrement
+                if (utilisateur != null){
+                    HttpSession httpSession = request.getSession();
+                    httpSession.setAttribute("isConnected", utilisateur);
+                    this.getServletContext().getRequestDispatcher("/ServletHome").forward(request, response);
+                }else{
+                    request.setAttribute("erreur", "Pas d'utilisateur");
+                    rd.forward(request, response);
+                }
+            }
+        }catch (BusinessException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/main/webapp/WEB-INF/jsp/inscription.jsp
+++ src/main/webapp/WEB-INF/jsp/inscription.jsp
@@ -6,12 +6,60 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Inscription</title>
-</head>
-<body>
-    <h1>Voila tu peux t'inscrire mais comme on as pas le temps on va dire que c'est ok</h1>
-    <a href="<%= request.getContextPath()%>/Connexion">CONNEXION</a>
-</body>
-</html>
+<%@ taglib prefix="core" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+    <html>
+    <jsp:include page="fragment/head.jsp">
+        <jsp:param name="title" value="Insciption - Encheres.org"/>
+    </jsp:include>
+    <body>
+    <%@include file="fragment/navbar.jsp"%>
+        <section class="section-inscription-form">
+            <form class="inscription-form" action="<%=request.getContextPath()%>/Inscription" method="post" >
+                <div class="row-form">
+                    <label class="form-label" for="pseudo"> Pseudo :</label>
+                    <input class="form-control" name="pseudo" id="pseudo" required placeholder="Votre pseudo">
+                    <label class="form-label" for="nom">Nom</label>
+                    <input class="form-control" name="nom" id="nom" required placeholder="Votre nom" >
+                </div>
+                <div class="row-form">
+                    <label class="form-label" for="prenom"> Prenom : </label>
+                    <input class="form-control" name="prenom" id="prenom" required placeholder="Votre prenom">
+                    <label class="form-label" for="email">Email :</label>
+                    <input class="form-control" type="email" name="email" id="email" required placeholder="Votre email" >
+                </div>
+                <div class="row-form">
+                    <label class="form-label" for="telephone"> Telephone : </label>
+                    <input class="form-control" name="telephone" id="telephone" required placeholder="Votre telephone">
+                    <label class="form-label" for="rue">Rue : </label>
+                    <input class="form-control" name="rue" id="rue" required placeholder="Votre rue" >
+                </div>
+                <div class="row-form">
+                    <label class="form-label" for="ville"> Ville :</label>
+                    <input class="form-control" name="ville" id="ville" required placeholder="Votre ville">
+                    <label class="form-label" for="codePostal">Code Postal</label>
+                    <input class="form-control" name="codePostal" id="codePostal" required placeholder="Votre code Postal" >
+                </div>
+                <div class="row-form">
+                    <label class="form-label" for="psw"> Mot de passe : </label>
+                    <input class="form-control" type="password" name="psw" id="psw" required placeholder="Votre Mot de passe">
+                    <label class="form-label" for="pswConf">Confirmation : </label>
+                    <input class="form-control" type="password" name="pswConf" id="pswConf" required placeholder="Votre Confirmation mot de passe" >
+                </div>
+
+                <div class="row-form-btn">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                    <a href="<%= request.getContextPath()%>/ServletHome">
+                        <button type="button" class="btn btn-danger">Annuler</button>
+                    </a>
+
+
+                </div>
+
+            </form>
+        </section>
+
+    <%@include file="fragment/footer.jsp"%>
+    </body>
+    </html>
