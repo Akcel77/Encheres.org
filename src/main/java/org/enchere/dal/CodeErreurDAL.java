b427e6d81f168d99a50a74b7a3928298f43c36e8
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-06-09 22:33:34 +0200

    CodeErreurDAL start

---
+++ src/main/java/org/enchere/dal/CodeErreurDAL.java
@@ -0,0 +1,9 @@
+package org.enchere.dal;
+
+public abstract class CodeErreurDAL {
+
+    /**
+     * Probleme d'insertion utilisateur BDD
+     */
+    public static final int ERREUR_UTILISATEUR_INSERT = 20000;
+}
