4211e8e15403219176099075f4121f2e2bad9422
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-06-12 00:43:15 +0200

    Servlet de connection a tester

--- src/main/java/org/enchere/servlet/enchere/ServletListeEncheres.java
+++ src/main/java/org/enchere/servlet/enchere/ServletListeEncheres.java
@@ -1,12 +1,24 @@
 package org.enchere.servlet.enchere;
 
+import org.enchere.bll.ArticleManager;
+import org.enchere.bo.Articles;
+import org.enchere.outils.BusinessException;
+
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 
 /**
  * Permet de lister les enchères en cours en mode déconnecté
@@ -30,7 +42,33 @@ public class ServletListeEncheres extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //l'utilisateur filtre la recherche
+        HttpSession httpSession = request.getSession();
+        if (httpSession.getAttribute("isConnected") != null){
+
+            try {
+                if (request.getParameter("encheres") != null && request.getParameter("ventes") != null){
+                    //TODO redirection
+                    System.out.println("erreur");
+                }else if(request.getParameter("encheres") != null || request.getParameter("ventes") != null){
+                    String checkbox;
+                    if (request.getParameter("encheres") != null){
+                        checkbox="encheres";
+                    }else {
+                        checkbox="ventes";
+                    }
+                    request.setAttribute("encheres", selectCond(request.getParameter("nom"), Integer.parseInt(request.getParameter("categorie")),request.getParameter(checkbox), (int ) httpSession.getAttribute("isConnected")));
+                }
+
+
+                request.setAttribute("utilisateur", httpSession.getAttribute("isConnected"));
+                request.setAttribute("articles", ArticleManager.findAll());
+                this.getServletContext().getRequestDispatcher("/WEB_INF/jsp/enchereLog.jsp").forward(request,response);
+
+            }catch (SQLException | BusinessException b){
+                b.printStackTrace();
+            }
+        }
+
     }
 
 
