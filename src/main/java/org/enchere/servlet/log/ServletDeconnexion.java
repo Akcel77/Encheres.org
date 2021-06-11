8325b7ca95f1b9d23795889d2e51abb59f6301cf
Modji <jeanbaptiste.cochinard@gmail.com>
2021-06-11 21:17:40 +0200

    Ajout dun doGet dans le doPost

--- src/main/java/org/enchere/servlet/log/ServletDeconnexion.java
+++ src/main/java/org/enchere/servlet/log/ServletDeconnexion.java
@@ -20,6 +20,6 @@ public class ServletDeconnexion extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 }
